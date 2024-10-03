64030663830aeea0133092ea63739587c6f7e8f7
Snoe <yurikorolev99@gmail.com>
2024-10-03 08:20:05 -0400

    created project

---
+++ logic-and-loops/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace logic_and_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
